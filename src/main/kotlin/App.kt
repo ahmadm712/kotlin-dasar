8f7a70cb3290b5da21f91b33df3d573ca5091798
ahmadm712 <ahmadmuji2808@gmail.com>
2022-10-19 10:17:08 +0700

    setup project

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+//    Var is dynamic so we can reassigned value of variable
+    val name = "Ahmad Muji"
+    var company : String = "Dicoding"
+    company = "dicoding academy"
+//    Val can't reassigned value in variable
+    val work : String = "Mobile dev "
+//    work = "Flutter dev"
+
+
+//    Operator
+    val valueA: Int = 10
+    val valueB: Int = 20
+
+    val result : Int = valueA + valueB
+
+
+    println("----LOG----")
+    println(result)
+    println("---------")
+    println("Ahmad Muji sebagai ${work + company}")
+}
